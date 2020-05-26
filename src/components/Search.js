2479046d2d391e01d2bf8c3c0957501682f9bf12
Blake <bpro0821@gmail.com>
2020-05-25 23:29:04 -0500

    returning tweet information for TweetDeck view

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,73 +1,75 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 import { Button} from 'react-bootstrap';
-
-import axios from 'axios';
+import { GlobalContext } from '../context/GlobalState';
 
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 const PROXY = 'https://cors-anywhere.herokuapp.com/'
 
 const Search = () => {
+  const { addTweets } = useContext(GlobalContext);
+  const { deleteTweet } = useContext(GlobalContext);
+// Symol options for search bar
   const [options, setOptions] = useState([]);
+// User-selected input for API requests
   const [selected, setSelected] = useState([]);
-  
+  const [symbolData, setSymbolData] = useState([]);
 
-  const handleSearch = useCallback((query) => {
-    fetch(`${PROXY}https://api.stocktwits.com/api/2/search/symbols.json?&q=${query}`)
-    .then(response => response.json())
-    .then(data => setOptions((data.results)))
-});
+  const cashtags = selected.map((selection) => selection.symbol);
 
-const handleSubmit = () => {
+async function handleSearch  (query)   {
+    const requests = cashtags.map(async(cashtag) => 
+      await fetch(
+        `${PROXY}https://api.stocktwits.com/api/2/streams/symbol/${cashtag}.json`
+      )
+    )
+      await fetch(
+      `${PROXY}https://api.stocktwits.com/api/2/search/symbols.json?&q=${query}`
+    )
+      .then ((response) => response.json())
+      .then((data) => setOptions(data.results))
+      return await Promise.all(requests)
+      // map array of responses into an array of response.json() to read their content
+      .then((responses) => Promise.all(responses.map((res) => res.json())))
+      // all JSON info is parsed
+      .then((data) => setSymbolData(data.map(symbolData=> symbolData)))
+      // symbolData will be an array of objects for manipulation elsewhere in our application. 
+    
+  };
 
-// iterate over the selected stocks for our next API call (returning data for each symbol selected)
-const cashtags = selected.map(selection => selection.symbol)
-
-
-// initialize empty array that will hold our unique links for axios.all requests
-let dynamicLinks = [];
-
-// iterate over the cashtags array and push a link containing a unique cashtag to the dynamicLinks array.
-cashtags.forEach(function(cashtag){
-    dynamicLinks.push(`${PROXY}https://api.stocktwits.com/api/2/streams/symbol/${cashtag}.json`)
-})
-
-console.log("dynamicLinks",dynamicLinks);
-
-axios.all(dynamicLinks.map(link => axios.get(link)))
-.then(axios.spread(function (...res) {
-    console.log("response", res);
-    {console.log("cashtags", cashtags)}
-    console.log("from", Array.from(dynamicLinks))
-}));
-};
-
+  useEffect(() => {
+    if(symbolData){
+      addTweets(symbolData);
+    }
+  }, [symbolData]);
 
   return (
-      <>
-      {console.log("selected",selected.map(selection => selection.symbol))}
-    <AsyncTypeahead
-      id="stocktwits"
-      clearButton
-      labelKey= "symbol"
-      multiple
-      useCache
-      clearButton
-      emptyLabel="No matches found"
-      minLength={1}
-      selected={selected}
-      onChange={setSelected}
-      options={options}
-      onSearch={handleSearch}
-      placeholder="Search by cashtag..."
-      renderMenuItemChildren={(option, props) => (
-        <div>
-          <span>{option.title}</span>
-        </div>
-      )}
-    />
-    <Button onClick={ ()=> selected.length > 0 ? handleSubmit() : alert("None selected!")} variant="light">Submit</Button>
+    <>
+      <AsyncTypeahead
+        isLoading={false}
+        id="stocktwits"
+        clearButton
+        labelKey="symbol"
+        multiple
+        promptText={"Must input a minimum of 2 characters"}
+        searchText={"Searching"}
+        useCache
+        onChange={setSelected}
+        filterBy={['symbol', 'title']}
+        emptyLabel="No matches found"
+        minLength={2}
+        selected={selected}
+        options={options}
+        onSearch={handleSearch}
+        placeholder="Enter cashtag or company name.."
+        renderMenuItemChildren={(option, props) => (
+          <div>
+            <Button variant="link">{option.title}</Button>
+          </div>
+        )}
+      />
+      {console.log("$tags", cashtags)}
     </>
   );
 };
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,15 +1,16 @@
 export default (state, action) => {
 
     switch(action.type) {
+
         case 'DELETE_TWEET':
             return{
                 ...state,
                 tweets: state.tweets.filter(tweet => tweet.id !== action.payload)
             }
-        case 'PULL_TWEETS':
+        case 'ADD_TWEETS':
             return{
                 ...state,
-                tweets: [ action.payload, ...state.tweets ]
+                tweets: action.payload, ...state.tweets 
             }
         default:
             return state;
