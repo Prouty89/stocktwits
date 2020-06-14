bb266d31e14482e99f3089b615c098e42c67f517
Blake <bpro0821@gmail.com>
2020-06-13 19:32:20 -0500

    add created_at, @user to message

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,32 +1,46 @@
 import React from "react";
 
 
-import { ListGroup, Card } from "react-bootstrap";
+import { ListGroup, Card, Badge } from "react-bootstrap";
 
 const TweetCard = (symbol) => {
-  const cardTitle = symbol.title.symbol.symbol;
+  const stockSymbol = symbol.title.symbol.symbol;
+  const stockName = symbol.title.symbol.title;
+  const user = symbol.title.messages.map((users)=> users.user);
 
   const messages = symbol.title.messages;
 
+//   const users = user.map((user) => {
+//       return <h1>{user.username}</h1>
+//   });
+
   const bodies = [];
+
 
   for (const [index, value] of messages.entries()) {
     bodies.push(
-      <ListGroup className="list-group-flush" key={index}>
+      <ListGroup.Item variant="light" key={index}>
+        <div className="tweet-text"> 
         {value.body}
-      </ListGroup>
+        <Badge variant="light">
+        @{value.user.username}
+        </Badge>
+        <Badge variant="light">
+        {new Date(value.created_at.replace(/(?=[AP]M)/i, ' ')).toLocaleString()}{''}
+        </Badge>
+        </div>
+      </ListGroup.Item>
     );
   }
 
   return (
     <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Text>{cardTitle}</Card.Text>
+      <Card style={{ width: "18rem" }} bg={"info"} className="mb-2" border="dark" text="white">
+  <Card.Header>{stockName}{''}(${stockSymbol})</Card.Header>
+        <ListGroup>
         {bodies}
-        <Card.Body>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
+        
+        </ListGroup>
       </Card>
     </>
   );
